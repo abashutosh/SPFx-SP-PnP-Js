9854f1772a4a18c289fde6a4f2b52d58301a9308
Ashutosh <agupta4@hitachiconsulting.com>
2019-01-04 16:00:48 +0530

    Initial commit

---
+++ SPFx_SpPnpJS_Solution/src/webparts/spFxPnPJsWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxPnPJsWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpFxPnPJsWebPartWebPartStrings' {
+  const strings: ISpFxPnPJsWebPartWebPartStrings;
+  export = strings;
+}
